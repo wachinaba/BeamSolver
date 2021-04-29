3e5be2b4f26e88ea2e0a0f35e9423d78e8fbff5f
wachinaba <wachinaba@gmail.com>
2021-04-30 07:07:22 +0900

    [sync]

--- beam.py
+++ beam.py
@@ -2,4 +2,23 @@ import sympy
 
 class BeamElement():
   def __init__(self):
-    pass
+    self.length = length
+    self.start = 0
+    
+    self.reaction_force = [None, None]
+    self.reaction_moment = [None, None]
+    self.external_force = [None, None]
+    self.external_moment = [None, None]
+    self.distributed_load = None
+
+    self.condition_diflection = [None, None]
+    self.condition_angle = [None, None]
+
+class Beam():
+  def __init__(self):
+    self.beams = list()
+    self.element_num = 0
+
+  def append(self, elem, type=None):
+    self.beams.append(elem)
+    self.element_num += 1

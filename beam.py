f3046505ed755e2f370e42d5ecbfde9eb6b9ef81
wachinaba <wachinaba@gmail.com>
2021-05-02 14:15:24 +0900

    before change

--- beam.py
+++ beam.py
@@ -2,18 +2,31 @@ import sympy
 from enum import Enum, auto
 
 class BeamElement():
+  class Force():
+    def __init__(self, strength, pos):
+      self.strength = strength
+      self.pos = pos
+
   def __init__(self, length, start, load=0):
     self.length = length
     self.start = start
     self.load = load
-    self.load_strength = 0
-    self.load_pos = 0
-    if load:
-
     self.shear_force = None
     self.moment = None
     self.angle = None
     self.deflection = None
+
+  def convert_load(self, x, start=self.start, end=(self.start + self.length)):
+    if not self.load:
+      return BeamElement.Force(0, 0)
+
+    integ = sympy.integrate(self.load, (x, start, end))
+    if not integ:
+      return BeamElement.Force(0, 0)
+
+    integ_x = sympy.integrate(self.load * x, (x, start, end))
+    
+    return BeamElement.Force(integ, integ_x / integ)
 
 class BeamNode():
   def __init__(self, rf=0, rm=0, ef=0, em=0):
@@ -41,9 +54,17 @@ class Beam():
     self.nodes = nodes
 
   def calc_balance(self):
+    self.balance_vertical = 0
+    self.balance_moment = 0
     for elem in self.elements:
-      self.balance_vertical += elem.convert_load(self.x)[0]
+      self.balance_vertical += elem.convert_load(self.x).strength
+      self.balance_moment += (elem.convert_load(self.x).strength * elem.convert_load(self.x).pos)
     for node in self.nodes:
-      
-
+      self.balance_vertical += (node.reaction_force + node.external_force)
+      self.balance_moment += (node.reaction_moment + node.external_moment)
+    
+  def calc_shear_force(self):
+  prev_expr = 0
+    for elem in self.elements:
+      self.
     
--- sympy_test.py
+++ sympy_test.py
@@ -1,11 +1,8 @@
 import sympy
 
 def main():
-  beam_num = 2
-  force_R_num = beam_num + 1
-  force_W_num = beam_num + 1
-
-  
+  integ = sympy.integrate(sympy.symbols("x"), (sympy.symbols("x"), 0, sympy.symbols("x")))
+  print(integ)
 
 if __name__ == "__main__":
   main()
